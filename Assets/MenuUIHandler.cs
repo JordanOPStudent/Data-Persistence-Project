e51427167808a83c60fc1504e1ac288830ea7c64
Salierisghost <salieri@salierisghost.xyz>
2022-07-13 12:05:43 +1200

    Added MenuUIHandler

---
+++ Assets/MenuUIHandler.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class MenuUIHandler : MonoBehaviour
+{
+    public InputField PlayerName;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartNewGame()
+    {
+        MainManager.Instance.CurrentPlayer = PlayerName.text;
+        SceneManager.LoadScene(1);
+    }
+
+    public void Exit()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
--- Assets/Scripts/MainManager.cs
+++ Assets/Scripts/MainManager.cs
@@ -1,24 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class MainManager : MonoBehaviour
 {
+    public static MainManager Instance;
+
     public Brick BrickPrefab;
     public int LineCount = 6;
     public Rigidbody Ball;
 
     public Text ScoreText;
     public GameObject GameOverText;
+    public Text HighScoreText;
+
+    public string CurrentPlayer = "";
     
     private bool m_Started = false;
     private int m_Points;
     
     private bool m_GameOver = false;
 
-    
+    private int m_bestScore = 0;
+    private string m_bestPlayer = "Name";
+
+    private void Awake()
+    {
+        // Prevents Duplicates
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        LoadData();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -36,6 +59,8 @@ public class MainManager : MonoBehaviour
                 brick.onDestroyed.AddListener(AddPoint);
             }
         }
+
+        SetBestPlayer();
     }
 
     private void Update()
@@ -68,9 +93,53 @@ public class MainManager : MonoBehaviour
         ScoreText.text = $"Score : {m_Points}";
     }
 
+    public void SaveScore(string name = "", int score = 0)
+    {
+        SaveData data = new SaveData();
+        data.Name = name;
+        data.BestScore = score;
+
+        string json = JsonUtility.ToJson(data);
+
+        File.WriteAllText(Application.persistentDataPath + "/savefile.json", json);
+    }
+
+    public void LoadData()
+    {
+        string path = Application.persistentDataPath + "/savefile.json";
+        if (File.Exists(path))
+        {
+            string json = File.ReadAllText(path);
+            SaveData data = JsonUtility.FromJson<SaveData>(json);
+
+            m_bestPlayer = data.Name;
+            m_bestScore = data.BestScore;
+        }
+    }
+
+    private void SetBestPlayer()
+    {
+        string highscoreText = "Best Score: " + m_bestPlayer.ToString() + ": " + m_bestScore.ToString();
+        HighScoreText.text = highscoreText;
+    }
+
     public void GameOver()
     {
         m_GameOver = true;
+        if (m_Points > m_bestScore)
+        {
+            m_bestPlayer = CurrentPlayer;
+            m_bestScore = m_Points;
+            SaveScore(CurrentPlayer, m_Points);
+            SetBestPlayer();
+        }
         GameOverText.SetActive(true);
+    }
+
+    [System.Serializable]
+    public class SaveData
+    {
+        public string Name;
+        public int BestScore;
     }
 }
